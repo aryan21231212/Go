0e6931af419bce0dec74c516c483636afe5e6cf8
aryan21231212 <aryanpratapsingh674@gmail.com>
2025-09-21 11:10:32 +0530

    slices

---
+++ 08myslices/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println("Slices in go")
+
+	myfruit := []string{"Apple", "Banana", "Grapes"}
+	fmt.Println("My fruit list is", myfruit)
+
+	// adding new fruit to the slice
+	myfruit = append(myfruit, "orange", "mango")
+	fmt.Println("My fruit list is", myfruit)
+	fmt.Printf("Type of fruit list %T \n", myfruit)
+
+	//slicing in slice
+	fmt.Println(myfruit[:3])
+
+	//creating slice using make
+	var mynumbers = make([]int, 4)
+	mynumbers[0] = 100
+	mynumbers[1] = 50
+	mynumbers[2] = 200
+	mynumbers[3] = 130
+
+	// mynumbers[4] = 400 // this will give error as the size of slice is 4
+
+	// to avoid this we use append function
+	mynumbers = append(mynumbers, 400, 500)
+	fmt.Println("My numbers: ", mynumbers)
+
+	sort.Ints(mynumbers)
+	fmt.Println(mynumbers)
+}
