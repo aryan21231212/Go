884e0c5376c43bcee9c8245ccc8029f4716a34aa
aryan21231212 <aryanpratapsingh674@gmail.com>
2025-09-30 22:59:45 +0530

    get req

---
+++ 20webreqverbs/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	fmt.Println("hello")
+	getrequest()
+}
+
+func getrequest() {
+	const myURL = "http://localhost:3000/get"
+
+	response, _ := http.Get(myURL)
+
+	fmt.Println("Status code:", response.StatusCode)
+	fmt.Println("Content length is: ", response.ContentLength)
+
+	defer response.Body.Close()
+
+	var reponseString strings.Builder
+	content, _ := io.ReadAll(response.Body)
+
+	byteCount, _ := reponseString.Write(content)
+	fmt.Println("Byte count is: ", byteCount)
+	fmt.Println("Response string is: ", reponseString.String())
+}
