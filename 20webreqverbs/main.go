5f850d95103cd7f26253c87f54c37ed3e44ecf55
aryan21231212 <aryanpratapsingh674@gmail.com>
2025-09-30 23:47:09 +0530

    Post

--- 20webreqverbs/main.go
+++ 20webreqverbs/main.go
@@ -9,23 +9,40 @@ import (
 
 func main() {
 	fmt.Println("hello")
-	getrequest()
+	// getrequest()
+	postrequest()
 }
 
 func getrequest() {
 	const myURL = "http://localhost:3000/get"
 
-	response, _ := http.Get(myURL)
+	// response, _ := http.Get(myURL)
 
-	fmt.Println("Status code:", response.StatusCode)
-	fmt.Println("Content length is: ", response.ContentLength)
+	// fmt.Println("Status code:", response.StatusCode)
+	// fmt.Println("Content length is: ", response.ContentLength)
 
-	defer response.Body.Close()
+	// defer response.Body.Close()
 
-	var reponseString strings.Builder
+	// var reponseString strings.Builder
+	// content, _ := io.ReadAll(response.Body)
+
+	// byteCount, _ := reponseString.Write(content)
+	// fmt.Println("Byte count is: ", byteCount)
+	// fmt.Println("Response string is: ", reponseString.String())
+}
+
+func postrequest() {
+	const myURL = "http://localhost:3000/post"
+
+	responseData := strings.NewReader(`{
+		"name": "Aryan",
+		"age":"20",
+		"year":"SE"
+	}`)
+
+	response, _ := http.Post(myURL, "application/json", responseData)
+
 	content, _ := io.ReadAll(response.Body)
-
-	byteCount, _ := reponseString.Write(content)
-	fmt.Println("Byte count is: ", byteCount)
-	fmt.Println("Response string is: ", reponseString.String())
+	defer response.Body.Close()
+	fmt.Println("Content", string(content))
 }
