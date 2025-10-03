7d66194b1a0ee2e9b9af91b9eced6a50526bfba5
aryan21231212 <aryanpratapsingh674@gmail.com>
2025-10-03 23:26:33 +0530

    decoding of json

--- 21bitmorejson/main.go
+++ 21bitmorejson/main.go
@@ -15,7 +15,8 @@ type course struct {
 
 func main() {
 	fmt.Println("hello")
-	encodeJson()
+	// encodeJson()
+	decodeJson()
 }
 
 func encodeJson() {
@@ -33,4 +34,41 @@ func encodeJson() {
 	}
 
 	fmt.Println(string(finalJson))
+}
+
+func decodeJson() {
+
+	jsonDataFromWeb := []byte(
+		`
+		 {
+                "coursename": "ReactJS Bootcamp",
+                "Price": 299,
+                "website": "learncodeonline.in",
+                "tags": ["web-dev","js"]
+        }
+		`)
+
+	checkValid := json.Valid(jsonDataFromWeb)
+	if checkValid {
+		fmt.Println("JSON was valid")
+		var coursera course
+		err := json.Unmarshal(jsonDataFromWeb, &coursera)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%#v\n", coursera)
+	} else {
+		fmt.Println("JSON was not valid")
+	}
+
+	// some cases where you just want to add data to key value
+
+	var myonlineData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myonlineData)
+	fmt.Printf("%#v\n", myonlineData)
+
+	for k, v := range myonlineData {
+		fmt.Printf("Key is %v and value is %v and type is %T\n", k, v, v)
+	}
+
 }
