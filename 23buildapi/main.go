c3c2b4120e49c214fba4ec3f1da59108759fe024
aryan21231212 <aryanpratapsingh674@gmail.com>
2025-10-04 15:46:26 +0530

    building api

---
+++ 23buildapi/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type Course struct {
+	CourseId    string  `json:"courseid"`
+	CourseName  string  `json:"coursename"`
+	CoursePrice int     `json:"price"`
+	Author      *Author `json:"author"`
+}
+
+type Author struct {
+	Fullname string `json:"fullname"`
+	Website  string `json:"website"`
+}
+
+// fake db
+var courses []Course
+
+//middleware - fileter, auth
+
+func (c *Course) isEmpty() bool{
+	return c.CourseName == "" && c.CourseId == "" 
+}
+
+
+func main() {
+	fmt.Println("hello")
+}
