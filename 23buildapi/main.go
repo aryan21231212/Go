8a3175e4a50f47ed0f8d435c255377f8c5e6209e
aryan21231212 <aryanpratapsingh674@gmail.com>
2025-10-04 15:56:35 +0530

    api route created

--- 23buildapi/main.go
+++ 23buildapi/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 type Course struct {
 	CourseId    string  `json:"courseid"`
@@ -19,11 +23,20 @@ var courses []Course
 
 //middleware - fileter, auth
 
-func (c *Course) isEmpty() bool{
-	return c.CourseName == "" && c.CourseId == "" 
+func (c *Course) isEmpty() bool {
+	return c.CourseName == "" && c.CourseId == ""
 }
-
 
 func main() {
 	fmt.Println("hello")
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>welcome to API</h1>"))
+}
+
+func getAllCources(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("getting all cources")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
 }
